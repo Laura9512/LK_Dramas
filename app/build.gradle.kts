e28c530ed3147ad74cf76c10d9d303d605a074be
lau12 <l.garcia18@alumnos.santotomas.cl>
2023-12-10 00:23:32 -0300

    versión 12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.santotomas_lauragarcia.lk_dramas"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.santotomas_lauragarcia.lk_dramas"
@@ -40,11 +40,24 @@ dependencies {
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-database:20.3.0")
-    implementation ("com.google.android.gms:play-services-maps:18.1.0")
-    implementation ("com.google.android.gms:play-services-location:20.0.0")
-    implementation ("org.osmdroid:osmdroid-android:6.1.8")
-    implementation ("org.osmdroid:osmdroid-wms:6.1.11")
+    implementation("com.google.android.gms:play-services-maps:18.1.0")
+    implementation("com.google.android.gms:play-services-location:20.0.0")
+    implementation("org.osmdroid:osmdroid-android:6.1.8")
+    implementation("org.osmdroid:osmdroid-wms:6.1.11")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //dependencias de mqtt
+    implementation("org.eclipse.paho:org.eclipse.paho.client.mqttv3:1.1.0")
+
+    implementation("com.android suppont:suppont-v4:28.0.01")
+    implementation("com.android.suppont:Localbroadcastmanagen:28.0.0")
+
+    implementation("org.eclipse.paho:org.eclipse.paho.android.service:1.1.1") {
+        exclude(group = "com.android.support")
+        exclude(module = "appcompat-v7")
+        exclude(module = "support-v4")
+
+    }
 }
